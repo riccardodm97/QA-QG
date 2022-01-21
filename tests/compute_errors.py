7e69689488e5877d1f5cd9a68ca7aed6e65c1340
JackBerselli <35583166+JackBerselli@users.noreply.github.com>
2022-01-21 13:26:27 +0100

    new compute_errors.py

--- tests/compute_errors.py
+++ tests/compute_errors.py
@@ -5,11 +5,12 @@ sys.path.insert(1, os.getcwd())
 
 import os 
 import pandas as pd
+import torch
+import numpy as np
 
-import lib.utils as utils 
-import lib.globals as globals
-import lib.data_handling as handling
-
+import src.utils as utils 
+import src.globals as globals
+import src.data_handler as handler
 
 from tokenizers import  Tokenizer
 from tokenizers.models import WordLevel
@@ -17,6 +18,7 @@ from tokenizers.normalizers import Lowercase, Sequence, Strip, StripAccents
 from tokenizers.pre_tokenizers import Punctuation
 from tokenizers.pre_tokenizers import Sequence as PreSequence
 from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.implementations.bert_wordpiece import BertWordPieceTokenizer
 from tokenizers import Encoding
 from datasets import Dataset 
 from tqdm import tqdm
@@ -26,20 +28,29 @@ class BuildErrorsDf:
 
         self.error_ids = open(os.path.join(globals.DATA_FOLDER,'error_ids.txt')).read().splitlines()
         self.dataframe_path_to_save = os.path.join(globals.DATA_FOLDER, 'error_df.csv')
-        squad_dataset = handling.RawSquadDataset(dataset_path)
-        self.df = squad_dataset.df.copy()
-        self.hf_dataset = Dataset.from_pandas(squad_dataset.df)
+        squad_dataset = handler.RawSquadDataset(dataset_path)
+        self.df = squad_dataset.train_df.copy()
+        self.hf_dataset_rnn = Dataset.from_pandas(squad_dataset.train_df)
+        self.hf_dataset_transformer = Dataset.from_pandas(squad_dataset.train_df)
         self.model, self.vocab = utils.load_embedding_model()
 
-        self.tokenizer = Tokenizer(WordLevel(self.vocab,unk_token=globals.UNK_TOKEN))
-        self.tokenizer.normalizer = Sequence([StripAccents(), Lowercase(), Strip()])
-        self.tokenizer.pre_tokenizer = PreSequence([Whitespace(), Punctuation()])
-        self.tokenizer.enable_padding(direction="right", pad_id=self.vocab[globals.PAD_TOKEN], pad_type_id=1, pad_token=globals.PAD_TOKEN)
+        self.tokenizer_rnn = Tokenizer(WordLevel(self.vocab,unk_token=globals.UNK_TOKEN))
+        self.tokenizer_rnn.normalizer = Sequence([StripAccents(), Lowercase(), Strip()])
+        self.tokenizer_rnn.pre_tokenizer = PreSequence([Whitespace(), Punctuation()])
+        self.tokenizer_rnn.enable_padding(direction="right", pad_id=self.vocab[globals.PAD_TOKEN], pad_type_id=1, pad_token=globals.PAD_TOKEN)
 
-    def transform(self, batch):
+        VOCAB_PATH = os.path.join(globals.DATA_FOLDER,globals.BERT_PRETRAINED+'-vocab.txt')
+        if not os.path.exists(VOCAB_PATH):
+            utils.load_bert_vocab()
 
-        context_encodings: list[Encoding] = self.tokenizer.encode_batch(batch['context'])
-        answer_encodings: list[Encoding] = self.tokenizer.encode_batch(batch['answer'])
+        self.tokenizer_transformer = BertWordPieceTokenizer(VOCAB_PATH, lowercase=True)
+        self.tokenizer_transformer.enable_padding(direction="right", pad_type_id=1)
+        self.tokenizer_transformer.enable_truncation(globals.BERT_MAX_TOKENS, strategy='only_second', stride = 25)
+
+    def transform_rnn(self, batch):
+
+        context_encodings: list[Encoding] = self.tokenizer_rnn.encode_batch(batch['context'])
+        answer_encodings: list[Encoding] = self.tokenizer_rnn.encode_batch(batch['answer'])
         
         starts = list(map(lambda x: x[0],batch['label_char']))
         ends = list(map(lambda x: x[1],batch['label_char']))
@@ -49,57 +60,121 @@ class BuildErrorsDf:
             'context_text': batch['context'],
             'question_text': batch['question'],
             'answer_text': batch['answer'],
-            'label_token_start': [e.char_to_token(starts[i]) for i,e in enumerate(context_encodings)],
-            'label_token_end': [e.char_to_token(ends[i]-1) for i,e in enumerate(context_encodings)],
-            'label_char_start': starts,
-            'label_char_end': ends,
+            'token_start_rnn': [e.char_to_token(starts[i]) for i,e in enumerate(context_encodings)],
+            'token_end_rnn': [e.char_to_token(ends[i]-1) for i,e in enumerate(context_encodings)],
+            'char_start_rnn': starts,
+            'char_end_rnn': ends,
             'question_id' : batch['question_id'],
             'context_tokens': [e.tokens for e in context_encodings], 
-            'answer_tokens': [e.tokens for e in answer_encodings]
+            'answer_tokens': [e.tokens for e in answer_encodings],
         }
 
         return encodings
+
+    def transform_transformer(self, batch):
+
+            encodings: list[Encoding] = self.tokenizer_transformer.encode_batch(list(zip(batch['question'],batch['context'])))
+
+            starts = list(map(lambda x: x[0],batch['label_char']))
+            ends = list(map(lambda x: x[1],batch['label_char']))
+
+            not_replaced = []
+            for i,e in enumerate(encodings) :
+                if e.char_to_token(starts[i],1) is None or e.char_to_token(ends[i]-1,1) is None :
+                    flag = 0
+                    for o in e.overflowing :
+                        if o.char_to_token(starts[i],1) is not None and o.char_to_token(ends[i]-1,1) is not None :
+                            encodings[i] = o
+                            flag = 1
+                            break
+                    if flag==0 :
+                        not_replaced.append(i)
+            
+            for idx in not_replaced:
+                encodings.pop(idx)
+                starts.pop(idx)
+                ends.pop(idx)
+                batch['context'].pop(idx)
+                batch['answer'].pop(idx)
+
+            batch = {
+                'offsets': [e.offsets for e in encodings], 
+                'type_ids': [e.type_ids for e in encodings],
+                'token_start_trans': [e.char_to_token(starts[i],1) for i,e in enumerate(encodings)],
+                'token_end_trans': [e.char_to_token(ends[i]-1,1) for i,e in enumerate(encodings)],
+                'context_text': batch['context'],
+                'question_text': batch['question'],
+                'answer_text': batch['answer'],
+                'char_start_trans' : starts,
+                'char_end_trans': ends,
+                'question_id' : batch['question_id']
+            }
+
+            return batch
     
-    def build_error_dataframe(self):
-        error_df_total = pd.DataFrame(columns=['context_text', 'question_text', 'answer_text', 'label_token_start', 
-                                        'label_token_end', 'label_char_start', 'label_char_end','question_id'])
+    def build_error_dataframe_rnn(self):
+        error_df_total = pd.DataFrame(columns=['context_text', 'question_text', 'answer_text', 'token_start_rnn', 
+                                        'token_end_rnn', 'char_start_rnn', 'char_end_rnn','question_id'])
 
         for i in tqdm(range(self.df.shape[0])):
 
-            ex = self.hf_dataset[i]
-            start_c = ex['label_char_start']
-            end_c = ex['label_char_end']
+            ex = self.hf_dataset_rnn[i]
+            start_c = ex['char_start_rnn']
+            end_c = ex['char_end_rnn']
             starts, ends = zip(*ex['offsets'])
-
-            if start_c not in starts and end_c in ends:
-                error_df_total = error_df_total.append(ex, ignore_index=True)
-            elif start_c in starts and end_c not in ends:
-                error_df_total = error_df_total.append(ex, ignore_index=True)
-            elif start_c not in starts and end_c not in ends:
-                error_df_total = error_df_total.append(ex, ignore_index=True)
-            elif ex['context_tokens'][starts.index(start_c)] != ex['answer_tokens'][0] or ex['context_tokens'][ends.index(end_c)] != ex['answer_tokens'][-1]:
+            if start_c in starts and end_c in ends:
+                continue
+            else:
                 error_df_total = error_df_total.append(ex, ignore_index=True)
 
         error_df_total.drop(['offsets','context_tokens','answer_tokens'], axis=1, inplace=True)
-        error_df_total['New'] = '✔'
+        error_df_total['Rnn New'] = '✔'
 
-        error_df = pd.DataFrame(columns=['context_text', 'question_text', 'answer_text', 'label_token_start', 
-                                        'label_token_end', 'label_char_start', 'label_char_end','question_id'])
+        error_df = pd.DataFrame(columns=['context_text', 'question_text', 'answer_text', 'token_start_rnn', 
+                                        'token_end_rnn', 'char_start_rnn', 'char_end_rnn','question_id'])
 
         for idx in self.error_ids:
-            ex = self.hf_dataset[int(self.df[self.df['question_id'] == idx].index.values.astype(int)[0])]
+            ex = self.hf_dataset_rnn[int(self.df[self.df['question_id'] == idx].index.values.astype(int)[0])]
 
             error_df = error_df.append(ex, ignore_index=True)
 
         error_df.drop(['offsets','context_tokens','answer_tokens'], axis=1, inplace=True)
-        error_df['Old'] = '✔'
+        error_df['Rnn Old'] = '✔'
 
         return error_df_total, error_df
 
+    def build_error_dataframe_transformer(self):
+        error_df_total = pd.DataFrame(columns=['context_text', 'question_text', 'answer_text', 'token_start_trans', 
+                                        'token_end_trans', 'char_start_trans', 'char_end_trans','question_id'])
+                                        
+        for i in tqdm(range(self.df.shape[0])):
+
+            ex = self.hf_dataset_transformer[i]
+
+            offsets = np.array(ex['offsets'])[np.array(ex['type_ids'], dtype=bool)]
+
+            start_c = ex['char_start_trans']
+            end_c = ex['char_end_trans']
+
+            starts, ends = zip(*offsets)
+
+            if start_c in starts and end_c in ends:
+                continue
+            else:
+                error_df_total = error_df_total.append(ex, ignore_index=True)
+        
+        error_df_total.drop(['offsets','type_ids'], axis=1, inplace=True)
+        error_df_total['Transformer'] = '✔'
+
+        return error_df_total
+
     def find_errors(self):
-        self.hf_dataset.set_transform(self.transform,output_all_columns=False)
-        error_df_total, error_df = self.build_error_dataframe()
-        error_df_total = error_df_total.merge(error_df, how='outer')
+        self.hf_dataset_rnn.set_transform(self.transform_rnn,output_all_columns=False)
+        self.hf_dataset_transformer.set_transform(self.transform_transformer,output_all_columns=False)
+        error_df_total_rnn, error_df_rnn = self.build_error_dataframe_rnn()
+        error_df_total_transformer = self.build_error_dataframe_transformer()
+        error_df_total_rnn = error_df_total_rnn.merge(error_df_rnn, how='outer')
+        error_df_total = error_df_total_rnn.merge(error_df_total_transformer, how='outer', on=['question_id','context_text','answer_text','question_text'])
         error_df_total.to_csv(self.dataframe_path_to_save)
 
 if __name__ == '__main__':
