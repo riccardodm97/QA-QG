155a77cb9ff5fbe9cf33a5b623bd3b3dbe8808a3
JackBerselli <35583166+JackBerselli@users.noreply.github.com>
2022-01-30 16:23:25 +0100

    run 7

--- src/exec_handler.py
+++ src/exec_handler.py
@@ -315,8 +315,8 @@ class QG_handler :
         DEC_HIDDEN = 256
         GRAD_CLIPPING = 10
         BATCH_SIZE = 64
-        LR = 0.005
-        DROPOUT = 0.5
+        LR = 0.002
+        DROPOUT = 0
         WEIGHT_DECAY = 0.01
         RANDOM_BATCH = False
 
